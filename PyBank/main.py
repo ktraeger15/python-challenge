58b3e22be4b0e2ea44410bd1ec3486a26155b396
ktraeger15 <kelseytraeger@Kelseys-MBP-2.attlocal.net>
2023-10-01 12:13:50 -0500

    Adding completed python code and output files git push

---
+++ PyBank/main.py
@@ -0,0 +1,71 @@
+# CSV reader and os
+import csv
+import os
+import statistics
+
+#Variable to count the number of months 
+num_months = 0
+#Variable to calculate total profits/losses
+profit_loss_count = 0
+
+#Lists to store the calculated differences and dates
+difference_list = []
+date_list = []
+
+#Stored value for calculating differences
+stored_val = 0
+
+#Stored values for greatest increase and decrease 
+greatest_inc = 0
+greatest_dec = 0
+
+#"/Users/kelseytraeger/Documents/python-challenge/PyBank/Resources/budget_data.csv"
+#csvpath = os.path.join("Resources", "budget_data.csv")
+with open("/Users/kelseytraeger/Documents/python-challenge/PyBank/Resources/budget_data.csv", "r") as csv_file: 
+    csv_reader = csv.reader(csv_file)
+
+    #Skip headers 
+    next(csv_reader)
+
+    for line in csv_reader: 
+        #Calculate total number of months included in the dataset
+        num_months += 1
+        #Changes in profit/losses over the entire period
+        profit_loss_count = profit_loss_count + int(line[1])
+        
+        #Add change in profits to a list and dates to list
+        difference_list.append((int(line[1]))-stored_val)
+        stored_val = int(line[1])
+        date_list.append(line[0])
+
+    #Removes the first item from the list
+    del difference_list[0]
+    del date_list[0]
+    #Loops though difference list to check for greatest increase and greatest decrease
+    for num in difference_list:
+        if num > greatest_inc:
+            greatest_inc = num
+        if num < greatest_dec:
+            greatest_dec = num
+    #Calculates average change
+    avg_change = statistics.mean(difference_list)
+    #Finds the dates from the greatest increase and greatest decrease from date_list using the index method
+    date_inc = date_list[difference_list.index(greatest_inc)]
+    date_dec = date_list[difference_list.index(greatest_dec)]
+
+#"/Users/kelseytraeger/Documents/python-challenge/PyBank/Analysis/Financial_Analysis.txt"
+#txtpath = os.path.join("Analysis", "Financial_Analysis.txt")
+file = open("/Users/kelseytraeger/Documents/python-challenge/PyBank/Analysis/Financial_Analysis.txt", "x")
+file.write(f"Financial Analysis \n")
+file.write(f"Total Months: {num_months}\n")
+file.write(f"Total: ${profit_loss_count}\n")
+file.write(f"Average Change: ${round(avg_change,2)}\n")
+file.write(f"Greatest Increase in Profits: {date_inc} ({greatest_inc})\n")
+file.write(f"Greatest Decrease in Profits: {date_dec} ({greatest_dec})\n")
+#Outputs
+print(f"Financial Analysis")
+print(f"Total Months: {num_months}")
+print(f"Total: ${profit_loss_count}")
+print(f"Average Change: ${round(avg_change,2)}")
+print(f"Greatest Increase in Profits: {date_inc} ({greatest_inc})")
+print(f"Greatest Decrease in Profits: {date_dec} ({greatest_dec})")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,56 @@
- 
+# CSV reader and os
+import csv
+import os
+import collections
+
+#Variables to count total votes cast and percentage votes
+total_vote = 0
+percentage_votes = 0
+
+#Counts votes for each candidate
+#Source: https://stackoverflow.com/questions/9247241/python-algorithm-of-counting-occurrence-of-specific-word-in-csv 
+#Source: https://docs.python.org/3/library/collections.html 
+candidate_votes_dict = collections.Counter()
+
+#"/Users/kelseytraeger/Documents/python-challenge/PyBank/Resources/budget_data.csv"
+#csvpath = os.path.join("Resources", "budget_data.csv")
+with open("/Users/kelseytraeger/Documents/python-challenge/PyPoll/Resources/election_data.csv", "r") as csv_file: 
+    csv_reader = csv.reader(csv_file)
+
+    #Skip headers 
+    next(csv_reader)
+
+    for line in csv_reader: 
+        #Creates a dictionary with candidate name as the key and total number of votes they received as the value 
+        #Source: https://stackoverflow.com/questions/9247241/python-algorithm-of-counting-occurrence-of-specific-word-in-csv
+        #Source: https://docs.python.org/3/library/collections.html 
+        candidate_votes_dict[line[2]] += 1
+
+for value in candidate_votes_dict.values():
+    #Calculates the total votes from dictionary
+    total_vote += value
+
+#"/Users/kelseytraeger/Documents/python-challenge/PyBank/Analysis/Election_Results.txt"
+#txtpath = os.path.join("Analysis", "Election_Results.txt")
+file = open("/Users/kelseytraeger/Documents/python-challenge/PyPoll/Analysis/Election_Results.txt", "x")
+
+#Outputs
+print(f"Election Results")
+file.write(f"Election Results \n")
+print(f"Total Votes: {total_vote}")
+file.write(f"Total Votes: {total_vote} \n")
+
+
+#Source: https://realpython.com/iterate-through-dictionary-python/#iterating-through-dictionaries-comprehension-examples
+for key, value in (candidate_votes_dict.items()):
+    #Calculates the percentage of the vote each candidate receives and formats it
+    percentage_votes = round((value/total_vote)*100,3)
+    #Prints the candidate name, the percentage of votes they received, and the number of votes they received
+    print(f"{key} {percentage_votes}% ({value})")
+    file.write(f"{key} {percentage_votes}% ({value})\n")
+    percentage_votes = 0
+
+#Prints winner
+#Source: https://www.w3resource.com/python-exercises/dictionary/python-data-type-dictionary-exercise-80.php#:~:text=Use%20max()%20with%20the,value%20in%20the%20given%20dictionary.
+print(f"Winner: {max(candidate_votes_dict, key=candidate_votes_dict.get)}")
+file.write((f"Winner: {max(candidate_votes_dict, key=candidate_votes_dict.get)}"))
